7511a27278551e60ae5af9ce8be251fd5df9a7a5
LMarcoccia <74210425+LMarcoccia@users.noreply.github.com>
2020-12-21 17:24:41 +0100

    Aggiunto il salvataggio dei grafici nella cartella results

--- NYC.py
+++ NYC.py
@@ -24,53 +24,55 @@ import tqdm
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--in_data", help = "path del file dataset",
-                     type = str, default = './Data/yellow_tripdata_2020-01.csv')
+parser.add_argument("-i1", "--mesi", help = "Mesi da analizzare",
+                     type = list, default = ['1', '2', '3', '4', '5', '6'])
 
-parser.add_argument("-i1", "--zone", help = "path del file dataset",
+parser.add_argument("-i2", "--zone", help = "Path del file dataset",
                      type = str, default = './Data/taxi+_zone_lookup.csv')
 
-#SISTEMARE IL PATH IN USCITA E CREARE LA CARTELLA RESULTS 
-parser.add_argument("-o", "--out_file", help = "cartella di deposito del file di uscita",
-                     type = str, default = './Results/{MESE}.csv')
+parser.add_argument("-i3", "--anno", help = "Anno da analizzare",
+                     type = int, default = 2020)
 
 args = parser.parse_args()
 
+#=============================================================================#
 
 start = time.perf_counter()
 
- 
-df = pd.read_csv(args.in_data,usecols = ['tpep_dropoff_datetime', 'DOLocationID'])
-
 
 zone = pd.read_csv(args.zone)
+zone = zone.rename({'LocationID': 'DOLocationID'}, axis=1)
 
 
-df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+for i in args.mesi: 
+    df = pd.read_csv(f'./Data/yellow_tripdata_2020-0{i}.csv',usecols = ['tpep_dropoff_datetime', 'DOLocationID'])
+
+
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 
 
 #elimino tutte le date diverse da 01/2020
-df = df.loc[(df['tpep_dropoff_datetime'].dt.year == 2020) & (df['tpep_dropoff_datetime'].dt.month  == 1)]
+    df = df.loc[(df['tpep_dropoff_datetime'].dt.year == args.anno) & (df['tpep_dropoff_datetime'].dt.month  == int(i))]
 
 
-df['tpep_dropoff_datetime'] = df['tpep_dropoff_datetime'].dt.date
+    df['tpep_dropoff_datetime'] = df['tpep_dropoff_datetime'].dt.date
+
+    
+    df = pd.merge(df, zone, on=['DOLocationID'],how='left')
 
 
-zone = zone.rename({'LocationID': 'DOLocationID'}, axis=1)
-df = pd.merge(df, zone, on=['DOLocationID'],how='left')
-
-
-dati_mese_zona=pd.DataFrame()
-for zona in df['Borough'].unique():
-    df_temp=df[df['Borough']==zona]
-    dati_mese_zona[zona] = pd.Series(collections.Counter(df_temp['tpep_dropoff_datetime']))
+    dati_mese_zona=pd.DataFrame()
+    for zona in df['Borough'].unique():
+        df_temp = df[df['Borough'] == zona]
+        dati_mese_zona[zona] = pd.Series(collections.Counter(df_temp['tpep_dropoff_datetime']))
     
 
-dati_mese_zona = dati_mese_zona.sort_index()
-for zona in df['Borough'].unique():
-    dati_mese_zona[zona].plot.bar()
-    plt.title(zona)
-    #plt.savefig(args.out_data) 
+    dati_mese_zona = dati_mese_zona.sort_index()
+    for zona in df['Borough'].unique():
+        plt.figure()
+        dati_mese_zona[zona].plot.bar()
+        plt.title(zona)
+        plt.savefig(f"./Results/2020-0{i}_{zona}.pdf") 
 
 
 elapsed = time.perf_counter() - start
